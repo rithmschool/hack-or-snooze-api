4afcdf80f6a040377812042bb31a5207a78f29cc
Michael Hueter <mthueter@gmail.com>
2017-11-13 13:01:21 -0800

    more access control headers

--- server/server.js
+++ server/server.js
@@ -34,9 +34,14 @@ app.use(bodyParserHandler); // error handling specific to body parser only
 // response headers setup
 app.use((request, response, next) => {
   response.header('Access-Control-Allow-Origin', '*');
-  response.header('Access-Control-Allow-Headers', 'Authorization');
-  response.header('Access-Control-Allow-Headers', 'Content-Type');
-  response.header('Access-Control-Allow-Methods', 'POST,GET,PATCH,DELETE,OPTIONS');
+  response.header(
+    'Access-Control-Allow-Headers',
+    'Origin, Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, Authorization'
+  );
+  response.header(
+    'Access-Control-Allow-Methods',
+    'POST,GET,PATCH,DELETE,OPTIONS'
+  );
   response.header('Content-Type', 'application/json');
   return next();
 });
